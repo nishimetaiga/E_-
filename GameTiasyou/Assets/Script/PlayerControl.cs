1955800d415bfb4faae8d3a1b6d4e1c864f0d37e
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-03-04 13:51:54 +0900

    ボートの移動処理を作成しました。

---
+++ GameTiasyou/Assets/Script/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //ボートの速さ
+    public float speed;
+    public float speed_up;
+    public float speed_down;
+    //ボートの曲がる角度
+    public float angle;
+
+
+    public GameObject player_z;     //プレイヤーの前のに行く方向
+
+
+    private void Update() {
+        //前に移動
+        if (Input.GetKey(KeyCode.W)) {
+            speed += Time.deltaTime*speed_up;
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player_z.transform.position, step);
+        }
+        else if(speed>0){
+            speed -= Time.deltaTime * speed_down;
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player_z.transform.position, step);
+
+        }
+        Debug.Log(transform.forward);
+        //右にカーブ
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(0.0f, 0.0f, angle);
+        }
+        else if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(0.0f, 0.0f, -angle);
+
+        }
+    }
+}
