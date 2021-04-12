fe1e1ac16894eb5a322ffceed280a0552c5fa636
hidetoo <hideto-oshiro-19h@stu.kbc.ac.jp>
2021-04-12 09:23:20 +0900

    ステージデータの保存　テスト

---
+++ GameTiasyou/Assets/oshiro/StageTest/DeleteFloor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteFloor : MonoBehaviour
+{
+    private Vector3 tmp;
+    public GameObject FloorObj;
+    private void Start()
+    {
+        tmp = this.transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //タグが　Player　のオブジェクトが触れたときに処理が行われる
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("すり抜けた！");
+            Instantiate(FloorObj, new Vector3(tmp.x, tmp.y, tmp.z - 50.0F), Quaternion.identity);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //タグが　Player　のオブジェクトが触れたときに処理が行われる
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("通り抜けた！");
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ GameTiasyou/Assets/oshiro/StageTest/TesttCameraPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesttCameraPosition : MonoBehaviour
+{
+    private Vector3 a;
+    private void FixedUpdate()
+    {
+        GameObject playerObj = GameObject.Find("TestPlayer");
+        a = playerObj.transform.position;
+    }
+    private void Update()
+    {
+        //StartCoroutine("Roto1");
+        transform.position = new Vector3(a.x , a.y + 20, a.z - 20);
+        transform.rotation = Quaternion.Euler(45, 0, 0);
+
+    }
+}
