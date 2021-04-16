6c0be1c376e6f905685c2f9eddc97358fdb3860b
nishimetaiga <taiga-nishime-19f@stu.kbc.ac.jp>
2021-04-16 13:08:23 +0900

    データ更新

---
+++ GameTiasyou/Assets/Game/C#/Player_gameover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Player_gameover : MonoBehaviour
+{
+
+    GameObject Player;
+
+    GameControl gmc;
+
+    //private void Start()
+    //{
+    //    Player= GameObject.Find("Player_2D ");
+    //    gmc = Player.GetComponent<GameControl>();
+    //}
+
+    //衝突判定
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "danmaku")
+        {
+            SceneManager.LoadScene("Result");
+            //Destroy(this.gameObject);
+        }
+    }
+}
---
+++ GameTiasyou/Assets/Game/C#/TimeBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeBar : MonoBehaviour
+{
+
+    private float time;
+
+    private float time2;//計算用
+
+
+    Slider timeSlider;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        time = 30f;
+        time2 = time;
+        //timer = GameObject.Find("Time").GetComponent<Text>();
+
+        timeSlider = GetComponent<Slider>();
+
+
+
+
+        //スライダーの最大値の設定
+        timeSlider.maxValue = time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time2 -= Time.deltaTime;
+
+        //timer.text = "Time:" + (time2 > 0f ? time2.ToString("0.00") : "0.00");
+
+        //スライダーの現在値の設定
+        timeSlider.value = time2;
+
+        if (time2 < 0)
+        {
+            SceneManager.LoadScene("Clear");
+        }
+
+    }
+}
