989acc77b371bcf5b6e542a66fb76e39cd4e14ad
Reihi-shinzato <65996577+Reihi-shinzato@users.noreply.github.com>
2021-04-22 20:36:27 +0900

    タイトルとリザルトのボタン押したときのSEを変えました。

---
+++ GameTiasyou/Assets/Game/C#/se_test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class se_test : MonoBehaviour
+{
+
+    void Start()
+    {
+     }
+    public void OnClick()
+    {  
+        Invoke("ChangeScene", 0.5f);
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
