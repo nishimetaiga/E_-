9340e1c51dba06d491ed20de36436d1d96bfff0d
nishimetaiga <taiga-nishime-19f@stu.kbc.ac.jp>
2021-05-10 10:08:26 +0900

    bgm追加

---
+++ GameTiasyou/Assets/Game/C#/BGM_con.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_con : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+
+    private bool flg=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 5")&&(flg==true)){
+            audioSource.Stop();
+            flg = false;
+
+        }else
+        if (Input.GetKeyDown("joystick button 5") && (flg == false))
+        {
+            audioSource.Play();
+            flg = true;
+
+        }
+
+        
+
+        
+    }
+}
